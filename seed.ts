6a95bf1bae25383e0b4731dd94c242848d8db9d0
shippokun <okashi0tabetai@gmail.com>
2021-01-10 11:32:18 +0900

    chore: add express
    起動して動作するあれこれ

---
+++ seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: 'test@user.com',
+      name: 'test',
+      posts: {
+        create: {
+          title: 'Test Post'
+        }
+      }
+    }
+  })
+
+  console.log({ user })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+  })
